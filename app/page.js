29f7808f66e248de03eeb18281c0d0d87e389f66
arshdeepweb <imarshdeep99@gmail.com>
2025-05-04 12:40:20 +0530

    Update Project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Navbar from "@/components/Navbar";
 import Portfolio from "@/components/Portfolio";
